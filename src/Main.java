b487005c3067b17a01ac7086d67dec71add25992
snnmyt <saxarnayaaa@gmail.com>
2023-09-06 22:51:44 +0300

    test2

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] argv) {
         System.out.println("Hello world");
         System.out.println("Podgorna Sophia");
-//hello hello
+        // hello hello
         
     }
 }
