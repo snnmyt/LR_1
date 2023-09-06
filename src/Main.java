a22a12ca62b62098d57ce227682b191a0f3aa6c4
snnmyt <110250389+snnmyt@users.noreply.github.com>
2023-09-06 22:49:04 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] argv) {
         System.out.println("Hello world");
         System.out.println("Podgorna Sophia");
-//hello
+//hello hello
+        
     }
 }
